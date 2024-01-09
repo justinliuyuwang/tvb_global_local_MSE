0e6511fc2750cf9849a8aed08a9b0825588737fa
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2024-01-09 15:21:33 -0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -12,25 +12,27 @@ paramfile=$1
 log_directory=$2
 
 
-min_G=$3
-max_G=$4
-num_G_values=$5
 
-min_noise=$6
-max_noise=$7
-num_noise_values=$8
+min_noise=$3
+max_noise=$4
+num_noise_values=$5
 
-min_Jn=${12}
-max_Jn=${13}
-num_Jn_values=${14}
 
-min_Ji=${15}
-max_Ji=${16}
-num_Ji_values=${17}
+min_G=$6
+max_G=$7
+num_G_values=$8
 
-min_Wp=${21}
-max_Wp=${22}
-num_Wp_values=${23}
+min_Jn=${9}
+max_Jn=${10}
+num_Jn_values=${11}
+
+min_Ji=${12}
+max_Ji=${13}
+num_Ji_values=${14}
+
+min_Wp=${15}
+max_Wp=${16}
+num_Wp_values=${17}
 
 
 # my_G=0.01
@@ -62,14 +64,42 @@ generate_values() {
 }
 
 # Generate parameter values
-G_values=($(generate_values $min_G $max_G $num_G_values))
 noise_values=($(generate_values $min_noise $max_noise $num_noise_values))
+G_values=($(generate_values $min_G $max_G $num_G_values))
 Jn_values=($(generate_values $min_Jn $max_Jn $num_Jn_values))
 Ji_values=($(generate_values $min_Ji $max_Ji $num_Ji_values))
 Wp_values=($(generate_values $min_Wp $max_Wp $num_Wp_values))
 
 # Create combinations and write them to the file
 > "$paramfile"  # Clear the file before writing
+
+
+# Varying Mi
+for my_noise in "${noise_values[@]}"; do
+    echo "$my_noise 0 0 0 0" >> "$paramfile"
+done
+
+# Varying Mi
+for my_G in "${G_values[@]}"; do
+    echo "0 $my_G 0 0 0" >> "$paramfile"
+done
+
+# Varying Mi
+for Jn in "${Jn_values[@]}"; do
+    echo "0 0 $Jn 0 0" >> "$paramfile"
+done
+
+# Varying Mi
+for Ji in "${Ji_values[@]}"; do
+    echo "0 0 0 $Ji 0" >> "$paramfile"
+done
+
+# Varying Mi
+for Wp in "${Wp_values[@]}"; do
+    echo "0 0 0 0 $Wp" >> "$paramfile"
+done
+
+                
 for my_G in "${G_values[@]}"; do
     for my_noise in "${noise_values[@]}"; do
                 # Varying Mi
@@ -94,7 +124,7 @@ for my_G in "${G_values[@]}"; do
 done
 
 # Read subjects and parameter combinations, then submit batch jobs
-    while read -r my_G my_noise Mi Jn Ji Wi We; do
+    while read -r my_noise my_G Jn Ji Wp; do
         sbatch -J "params_sim_G-${my_G}_noise-${my_noise}_Mi-${Mi}_Jn-${Jn}_Ji-${Ji}_Wi-${Wi}_We-${We}" -o "${log_directory}/params_sim_G-${my_G}_noise-${my_noise}_Mi-${Mi}_Jn-${Jn}_Ji-${Ji}_Wi-${Wi}_We-${We}.out" "sbatch.sh" "${my_G}" "${my_noise}" "${Mi}" "${Jn}" "${Ji}" "${Wi}" "${We}"
     done < "$paramfile"
 
