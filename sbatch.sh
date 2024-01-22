91f3f4b29499b68b1a43250b8788261d28f8f5be
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2024-01-21 17:41:41 -0700

    Update sbatch.sh

--- sbatch.sh
+++ sbatch.sh
@@ -2,14 +2,17 @@
 #SBATCH --account=rrg-rmcintos
 #SBATCH --mem=8000MB
 #SBATCH --time=0-3:00
+#SBATCH --array=1-100  # Example, replace 100 with the number of lines in your parameter file
 
+# Read parameters from file
+paramfile=$1
+log_directory=$2
+index=$SLURM_ARRAY_TASK_ID
+read -r noise G Jn Ji Wp <<< $(sed -n "${index}p" $paramfile)
 
-noise=${1} 
-G=${2}
-Jn=${3}
-Ji=${4}
-Wp=${5}
+# Log file naming based on parameters
+log_file="${log_directory}/params_sim_noise-${noise}_G-${G}_Jn-${Jn}_Ji-${Ji}_Wp-${Wp}.out"
 
-python single_sim_runner.py ${noise} ${G} ${Jn} ${Ji} ${Wp} 
-matlab -nodisplay -nosplash -nodesktop -r "noise=${noise}; G=${G}; Jn=${Jn}; Ji=${Ji}; Wp=${Wp}; run('mse');exit;"
-
+# Running simulations
+python single_sim_runner.py ${noise} ${G} ${Jn} ${Ji} ${Wp} > "$log_file"
+matlab -nodisplay -nosplash -nodesktop -r "noise=${noise}; G=${G}; Jn=${Jn}; Ji=${Ji}; Wp=${Wp}; run('mse');exit;" >> "$log_file"
