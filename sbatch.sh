0c7d63461492a809413156394dd6842ba872f2fa
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2024-01-21 19:04:50 -0700

    Update sbatch.sh

--- sbatch.sh
+++ sbatch.sh
@@ -3,17 +3,27 @@
 #SBATCH --mem=8000MB
 #SBATCH --time=0-3:00
 
-# Read parameters from file
 paramfile=$1
 log_directory=$2
-num_lines=$3  # This is the new argument passed from runner.sh
+num_simulations_per_job=$3
 index=$SLURM_ARRAY_TASK_ID
 
-read -r noise G Jn Ji Wp <<< $(sed -n "${index}p" $paramfile)
+# Calculate start and end line for this job
+start_line=$(( (index - 1) * num_simulations_per_job + 1 ))
+end_line=$(( index * num_simulations_per_job ))
 
-# Log file naming based on parameters
-log_file="${log_directory}/params_sim_noise-${noise}_G-${G}_Jn-${Jn}_Ji-${Ji}_Wp-${Wp}.out"
+# Ensure end line does not exceed the total number of lines
+total_lines=$(wc -l < "$paramfile")
+if [ $end_line -gt $total_lines ]; then
+    end_line=$total_lines
+fi
 
-# Running simulations
-python single_sim_runner.py ${noise} ${G} ${Jn} ${Ji} ${Wp} > "$log_file"
-matlab -nodisplay -nosplash -nodesktop -r "noise=${noise}; G=${G}; Jn=${Jn}; Ji=${Ji}; Wp=${Wp}; run('mse');exit;" >> "$log_file"
+# Loop over the specified range of lines
+for i in $(seq $start_line $end_line); do
+    read -r noise G Jn Ji Wp <<< $(sed -n "${i}p" $paramfile)
+    log_file="${log_directory}/sim_${i}_noise-${noise}_G-${G}_Jn-${Jn}_Ji-${Ji}_Wp-${Wp}.out"
+
+    # Run simulations and Matlab calls
+    python single_sim_runner.py ${noise} ${G} ${Jn} ${Ji} ${Wp} > "$log_file"
+    matlab -nodisplay -nosplash -nodesktop -r "noise=${noise}; G=${G}; Jn=${Jn}; Ji=${Ji}; Wp=${Wp}; run('mse');exit;" >> "$log_file"
+done
