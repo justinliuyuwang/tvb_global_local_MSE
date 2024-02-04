50bc32058d55e61fd5d19771d337632fc978a663
Justin Wang <32205576+justinliuyuwang@users.noreply.github.com>
2024-02-04 02:23:05 -0700

    Update model_wongwang.py

--- model_wongwang.py
+++ model_wongwang.py
@@ -36,31 +36,10 @@ def process_sub(my_noise,my_G,Jn,Ji,Wp):
     my_dt=1.0  # TO EDIT
 
     #start_time = time.time()
-    rww = models.DecoBalancedExcInh()
-    if not Jn==0:
-        print("Jn")
-        rww = models.DecoBalancedExcInh(J_N=np.array([Jn]))
-        my_noise=1e-5
-    elif not Ji==0:
-        print("Ji")
-        rww = models.DecoBalancedExcInh(J_i=np.array([Ji]))
-        my_noise=1e-5
-    elif not Wp==0:
-        print("Wp")
-        rww = models.DecoBalancedExcInh(w_p=np.array([Wp]))
-        my_noise=1e-5
-    elif not my_G==0:
-        print("G")
-        rww = models.DecoBalancedExcInh(G=np.array([my_G, ]))
-        my_noise=1e-5
-    elif not my_noise==0:
-        print("noise")
-        rww = models.DecoBalancedExcInh()
-        my_noise=my_noise
-    else:
-        print("default")
-        rww = models.DecoBalancedExcInh()
-        my_noise=1e-5
+    # rww = models.DecoBalancedExcInh()
+    rww = models.DecoBalancedExcInh(J_N=np.array([Jn]),J_i=np.array([Ji]),w_p=np.array([Wp]),G=np.array([my_G, ]))
+   
+
 
                         
     #my_G=0.01
@@ -74,7 +53,7 @@ def process_sub(my_noise,my_G,Jn,Ji,Wp):
         #connectivity=connectivity.Connectivity.from_file(),
         connectivity=get_connectivity(1),
         #coupling=coupling.Linear(a=numpy.array([0.5 / 50.0])),
-        coupling=coupling.Linear(a=np.array([0.00390625])),
+        coupling=coupling.Linear(a=np.array([0.00390625])), #calculated so that this x default G value in model = 0.01?
         integrator=integrators.EulerStochastic(dt=my_dt, noise=noise.Additive(nsig=np.array([my_noise]))), #1e-5
         monitors=(monitors.TemporalAverage(period=1.),),
         simulation_length=9e3
